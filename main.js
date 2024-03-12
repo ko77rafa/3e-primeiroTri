ceff961bdd8056506938cee3bd49276ff4497d02
rafael kott <126159276+ko77rafa@users.noreply.github.com>
2024-03-12 08:47:38 -0300

    g

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-const botoes = document.querySelectorAll(".botoes")
+const botoes = document.querySelectorAll(".botoes");
+for( int = 0 < botoes.length; j++;){
+    botoes{i} .onclick = function(){
+        for(int k = 0)
+    }
+    
+}
