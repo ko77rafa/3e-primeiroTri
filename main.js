a56293fd1324f3e2709f631abbd05527681221f2
rafael kott <126159276+ko77rafa@users.noreply.github.com>
2024-03-19 08:08:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const botoes = document.querySelectorAll(".botoes");
-for( int = 0 < botoes.length; j++;){
-    botoes{i} .onclick = function(){
-        for(int k = 0)
+for (let i = 0; i< botoes.length; i++){
+    botoes[i].onclick = function(){
+        for(let j = 0; j< botoes.length j++){
+         botoes[j].classList.remove("ativo");
+         }
+        botoes[i].classList.add("ativo")
     }
-    
-}
