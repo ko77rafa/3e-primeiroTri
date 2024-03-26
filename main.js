a33ba4aebbbf32b5fad7e9dde90022da622cdf7f
rafael kott <126159276+ko77rafa@users.noreply.github.com>
2024-03-26 07:16:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
- for (let i = 0; i < botoes.length; i++){
+const textos = document.querySelectorAll(".aba-conteudo");
+ for (let i = 0; i<botoes.length; i++){
    botoes[i].onclick = function(){
-for (let j = 0; j< botoes.length; j++) {
+for (let j = 0; j<botoes.length; j++) {
    botoes[j].classList.remove("ativo");
+   textos[j].classList.remove("ativo);
 }
- botoes[i].classList.add("ativo"); }     
+ botoes[i].classList.add("ativo");
+ textos[i].classList.add("ativo");
+   }     
  }
