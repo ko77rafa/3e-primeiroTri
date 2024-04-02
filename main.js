caeaac6c5aaa5c1d7dc5d08b20adc34ab19cf0e5
rafael kott <126159276+ko77rafa@users.noreply.github.com>
2024-04-02 07:56:56 -0300

    f

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
     botoes [i].onclick = function(){
        for (let j=0; j<botoes.length; j++){
          botoes[j].classList.remove("ativo");
-         textos[j].classList.remove("ativo);
+         textos[j].classList.remove("ativo");
 }
  botoes[i].classList.add("ativo");
  textos[i].classList.add("ativo");
