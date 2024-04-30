5a939f742bc8004f3073f311dc216683a707fc79
rafael kott <126159276+ko77rafa@users.noreply.github.com>
2024-04-30 08:46:32 -0300

    j

--- main.js
+++ main.js
@@ -27,8 +27,8 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
  function calculaTempo(tempoObjetivo){
   let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo1 - tempoAtual
-  let segundos = Math.floor(tempoFinal 1000);
+  let tempoFinal = tempoObjetivo1 - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
   let minutos = Math.floor(segundos/60);
   let horas = Math.floor(minutos / 60);
   let dias = Math.floor(horas / 24);
