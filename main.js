c098d6d91508956178e331bd19228c6eb83b7b5b
rafael kott <126159276+ko77rafa@users.noreply.github.com>
2024-04-30 08:00:23 -0300

    j

--- main.js
+++ main.js
@@ -28,6 +28,13 @@ const textos = document.querySelectorAll(".aba-conteudo");
   let minutos = Math.floor(segundos / 60);
   let horas = Math.floor(minutos / 60);
   let dias = Math.floor(horas / 24);
+
+  segundos %=60;
+  minutos %=60;
+  horas %=24;
+
+ return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+
  }
   
  
