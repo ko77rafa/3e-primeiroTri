6f6111112e381a14db69cc74937c2a0622a4619e
rafael kott <126159276+ko77rafa@users.noreply.github.com>
2024-04-30 08:16:29 -0300

    n

--- main.js
+++ main.js
@@ -3,29 +3,33 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
  for (let i = 0; i<botoes.length; i++) {
     botoes [i].onclick = function(){
-       for (let j=0; j<botoes.length; j++){
+       for (let j = 0; j<botoes.length; j++){
          botoes[j].classList.remove("ativo");
          textos[j].classList.remove("ativo");
 }
+
  botoes[i].classList.add("ativo");
  textos[i].classList.add("ativo");
    }     
  }
  const contadores = document.querySelectorAll(".contador");
  const tempoObjetivo1 = new Date (2025-01-28T23:59:59);
-  const tempos = [tempoObjetivo1]
+
+  const tempos = [
+    tempoObjetivo1
+  ]
 
  for (let i=0; i< contadores.length; i++){
     contadores[i].textContent = calculaTempo(tempos[i]);
   }
 
- contadores[0].textContent = calculaTempo(tempoObjetivo)1;
+ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
- function calculaTempo(tempoObjetivo1){
+ function calculaTempo(tempoObjetivo){
   let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo1 - tempoAtual;
-  let segundos = Math.floor(tempoFinal / 1000);
-  let minutos = Math.floor(segundos / 60);
+  let tempoFinal = tempoObjetivo1 - tempoAtual
+  let segundos = Math.floor(tempoFinal 1000);
+  let minutos = Math.floor(segundos/60);
   let horas = Math.floor(minutos / 60);
   let dias = Math.floor(horas / 24);
 
